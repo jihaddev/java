28cf80d2ccac450073bd60225f6d2a2e57faab77
jihaddev <144705695+jihaddev@users.noreply.github.com>
2023-09-11 21:07:22 +0600

    Math Class

---
+++ src/beginnerjava/MathClass.java
@@ -0,0 +1,15 @@
+package beginnerjava;
+
+public class MathClass {
+    public static void main(String[]args){
+        int x = 20;
+        int y = 10;
+        int max = Math.max(x,y);
+        System.out.println("Maximum = "+ max);
+        int mim = Math.min(x,y);
+        System.out.println("Minimum = "+ mim);
+
+
+
+    }
+}
