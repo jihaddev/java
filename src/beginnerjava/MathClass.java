baf5b3ec952c42432e4f9f5168d40ff8a726dc0e
jihaddev <144705695+jihaddev@users.noreply.github.com>
2023-09-11 21:28:41 +0600

    math class

--- src/beginnerjava/MathClass.java
+++ src/beginnerjava/MathClass.java
@@ -1,15 +1,24 @@
 package beginnerjava;
 
+import java.sql.SQLOutput;
+
 public class MathClass {
-    public static void main(String[]args){
+    public static void main(String[] args) {
         int x = 20;
         int y = 10;
-        int max = Math.max(x,y);
-        System.out.println("Maximum = "+ max);
-        int mim = Math.min(x,y);
-        System.out.println("Minimum = "+ mim);
-
-        System.out.println("princt");
-
+        int max = Math.max(x, y);
+        System.out.println("Maximum = " + max);
+        int mim = Math.min(x, y);
+        System.out.println("Minimum = " + mim);
+        int absolute = Math.abs(y);
+        System.out.println("Absoulte of y=" + absolute);
+        double power = Math.pow(x, y);
+        System.out.println("x to the power y=" + power);
+        int round = Math.round(8.4f);
+        System.out.println("Round of 8.4=" + round);
+        double pi = Math.PI;
+        System.out.println("pi=" + pi);
     }
 }
+
+
